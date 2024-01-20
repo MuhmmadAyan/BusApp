7f5c4bde3cb5d1795442a53692b6920569f60917
mohammad ayan <muhmmadayanashiq@gmail.com>
2024-01-20 19:51:00 +0530

    added the dropdown button

--- bus-app/src/App.js
+++ bus-app/src/App.js
@@ -4,11 +4,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import BusTrackingUI from './components/BusTrackingUI'; // Import the BusTrackingUI component
+import { Dropdown } from 'react-bootstrap';
+import Buses from './components/buses';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Buses/>
       <BusTrackingUI /> {/* Include the BusTrackingUI component */}
     </>
   );
